3436fb802adbf6435861f2f9dc11f5b7e35f2e7d
Rohit Chachra <ue228087rohit@gmail.com>
2025-05-03 07:26:41 +0000

    add in the footer about wikipeidia api also

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,8 +5,9 @@ export function Footer() {
       <div className="container py-4 text-center text-sm text-muted-foreground">
         <p>
           Powered by
-          <a href="https://www.desmos.com/api/v1.8/docs/index.html" target="_blank" rel="noopener noreferrer" className="underline hover:text-primary"> Desmos API</a>, and
-          <a href="https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest" target="_blank" rel="noopener noreferrer" className="underline hover:text-primary"> PubChem PUG REST API</a>.
+          <a href="https://www.desmos.com/api/v1.8/docs/index.html" target="_blank" rel="noopener noreferrer" className="underline hover:text-primary px-1"> Desmos API</a>,
+          <a href="https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest" target="_blank" rel="noopener noreferrer" className="underline hover:text-primary px-1"> PubChem PUG REST API</a>, and
+          <a href="https://en.wikipedia.org/api/rest_v1/" target="_blank" rel="noopener noreferrer" className="underline hover:text-primary px-1"> Wikipedia API</a>.
         </p>
         <p>© {new Date().getFullYear()} PCMBtools. All rights reserved.</p>
       </div>
